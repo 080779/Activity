a0779cb9ddd40d3a5c0e40356124afb006a45c97
080779 <exceed295@qq.com>
2017-11-19 19:59:59 +0800

    235123

--- Chat.FrontWeb/Models/user/AddUser.cs
+++ Chat.FrontWeb/Models/user/AddUser.cs
@@ -12,11 +12,11 @@ namespace Chat.FrontWeb.Models.user
         [StringLength(60,MinimumLength =2,ErrorMessage ="姓名要在2到30个字之间")]
         public string Name { get; set; }
         [Required(ErrorMessage = "手机号必须填")]
-        [StringLength(60, MinimumLength = 2, ErrorMessage = "姓名要在2到30个字之间")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "请输入11位手机号")]
         public string Mobile { get; set; }
         public bool Gender { get; set; }
-        [Required(ErrorMessage = "用户名必须填")]
-        [StringLength(60, MinimumLength = 2, ErrorMessage = "姓名要在2到30个字之间")]
+        [Required(ErrorMessage = "地址必须填")]
+        [StringLength(256, MinimumLength = 2, ErrorMessage = "地址要大于个字之间")]
         public string Address { get; set; }
     }
 }
